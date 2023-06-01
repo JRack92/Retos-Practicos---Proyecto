fe8cdbc53c4d67919373646272f0a78aee386c36
Jorge <rack1992@gmail.com>
2023-06-01 17:05:36 -0500

    Se actualiza EncuentasGame

---
+++ retos/EncuestaGame/main.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import "./index.js";
+
+document.querySelector("#app").innerHTML = `
+<div id="StageContainer">
+    <div class="StageCardContainer hidden">
+    </div>
+    <div class="StageView hidden">
+    </div>
+</div>
+`;
